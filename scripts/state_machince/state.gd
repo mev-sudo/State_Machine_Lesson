1c22ce6e025194a6473e6997c2b2a2b8ea338dfc
mev-sudo <mev@cathcollege.school.nz>
2026-06-16 12:11:31 +1200

    Added state machine
    Created the base State machine to Godot files, and player.

---
+++ scripts/state_machince/state.gd
@@ -0,0 +1,17 @@
+class_name State extends Node
+
+
+signal switch_state(state: State)
+
+func enter_state () -> void:
+	pass
+	
+	
+func exit_state () -> void:
+	pass
+	
+func update (_delta: float) -> void: 
+	pass
+	
+func phyics_update (_delta: float) -> void:
+	pass
